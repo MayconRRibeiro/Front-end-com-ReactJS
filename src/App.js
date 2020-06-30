5868156d01fe25efcbe5f765bd904ee193174bb3
Mike <m1kros@hotmail.com>
2020-06-29 23:16:52 -0300

    Estado e Imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 /**
  * Componente
- * Propriedade: Informação que queremos passar de um componente pai para um componente filho.
- * Estado
+ * Propriedade
+ * Estado & Imutabilidade
  */
 
 function App() {
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  function handleAddProject() {
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+  }
+
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul>
-      </Header>
+      <Header title='Projects'></Header>
+
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
     </>
   );
 }
